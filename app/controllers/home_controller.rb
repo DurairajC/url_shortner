f070cd3a98fc104d5003fda6dd7a339e58a3eb08
durairaj <durai.knr@gmail.com>
2018-09-24 22:24:17 +0530

    First commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,22 @@
+class HomeController < ApplicationController
+
+  def index
+	
+	username = "o_3ciin9m2d1"
+    api_key = "R_801deaa5a87049458a7e22e349d1d863"
+
+	begin 
+	
+	if params[:url_short] 	
+		bitly = Bitly.new(username, api_key)
+		@url = bitly.shorten(params[:url_short][:name])
+	end
+	
+	rescue
+		flash[:notice] = 'Enter correct url to shorten'
+		redirect_to root_path
+	end
+  
+  end
+   
+end
